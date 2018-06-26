eb0a5f41f094fcf071324df4e784a74540daa5e1
Athura <joshualjohnson33@gmail.com>
2018-06-26 09:02:19 -0400

    Boilerplate react code + hot module reload

--- routes/restauraunt-routes.js
+++ routes/restauraunt-routes.js
@@ -3,21 +3,17 @@ const router = express.Router();
 const jwt = require('jsonwebtoken');
 const passport = require('passport');
 const keys = require('../config/keys');
+const yelp = require('yelp-fusion');
 
 const Restaurant = require('../models/restaurant-model');
 const validateRestaurantInput = require('../validators/restaurant-validator');
+
+const client = yelp.client(keys.yelpClientKey);
 
 // @route GET api/restaurants/test
 // @desc Test restaurant functionality
 // @access Public
 router.get('/test', (req,res) => res.json({ msg: 'Restaurants work' }));
-
-// @route GET api/restaurants/yelp
-// @desc Search Yelp restaurants
-// @access Public
-router.get('/yelp', (req, res) => {
-    
-})
 
 
 // @route   POST api/restaurants/follow
