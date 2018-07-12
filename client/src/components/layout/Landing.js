44237ce9192ba6a000a75cac60bec638db948069
Joshua <joshualjohnson33@gmail.com>
2018-07-12 19:26:14 -0400

    css please

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -10,6 +10,12 @@ const Button = styled.button`
   background: transparent;
   color: palevioletred;
   border: 2px solid palevioletred;
+  cursor: pointer;
+
+  position: absolute;
+  left: 50%;
+  margin-left: -100px;
+  margin-top: 550px;
 
   ${props => props.primary && css`
     background: palevioletred;
@@ -17,12 +23,28 @@ const Button = styled.button`
   `}
 `;
 
+const Header = styled.header`
+  position: absolute;
+  left: 50%;
+  margin-left: -135px;
+  margin-top: 500px;
+
+  ${props => props.primary && css`
+    text-transform: uppercase;
+    color: white;
+    font-size: 30px;
+    margin-bottom: 40px;
+  `}
+`
+
 class Landing extends Component {
   render() {
     return (
       <div className="background">
-        <Button>Normal Button</Button>
-        <Button primary></Button>
+          <div className="container">
+              <Header primary className="landing_header">Bingo Bongo I'm a header</Header>
+              <Button>Props created button</Button>
+          </div>
       </div>
     )
   }
